d5b0bed8ec59b82d946781bbbf437f36aa211b93
like <zhenglike@huawei.com>
2023-10-10 16:54:35 +0800

    .

---
+++ t.ts
@@ -0,0 +1,10 @@
+function f45() {
+  return 0;
+  class C113 {
+    [Symbol.iterator]() {
+      return {
+        next() {},
+      };
+    }
+  }
+}
--- test.ts
+++
@@ -1,3 +0,0 @@
-function f59(): number[] {}
-class C60 extends f59 {}
-const v75: number = new C60();
