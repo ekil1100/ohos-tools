9b6a34a5253fc3f203eee8a154cd3e5e6689c6df
like <zhenglike@huawei.com>
2023-11-14 21:51:29 +0800

    .

--- t.ts
+++ t.ts
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) 2023 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -15,46 +15,165 @@
 declare function print(arg: any, arg1?: any): string;
 
 // print(`Promise: ${Promise}`);
-// globalThis.Promise = "Promise";
+// globalThis.Promise = 
 // print(`Promise: ${Promise}`);
 
 // print(`Proxy: ${Proxy}`);
-// globalThis.Proxy = "Proxy";
+// globalThis.Proxy = 
 // print(`Proxy: ${Proxy}`);
 
 // print(`Number: ${Number}`);
-// globalThis.Number = "Number";
+// globalThis.Number = 
 // print(`Number: ${Number}`);
 
 // print(`Object: ${Object}`);
-// globalThis.Object = "Object";
+// globalThis.Object = 
 // print(`Object: ${Object}`);
 
 // print(`Boolean: ${Boolean}`);
-// globalThis.Boolean = "Boolean";
+// globalThis.Boolean = 
 // print(`Boolean: ${Boolean}`);
 
 // print(`RegExp: ${RegExp}`);
-// globalThis.RegExp = "RegExp";
+// globalThis.RegExp = 
 // print(`RegExp: ${RegExp}`);
 
 // print(`String: ${String}`);
-// globalThis.String = "String";
+// globalThis.String = 
 // print(`String: ${String}`);
 
 // print(Array);
-// globalThis.Array = "Array";
+// globalThis.Array = 
 // print(Array);
 
 // print(`Function: ${Function}`);
-// globalThis.Function = "Function";
+// globalThis.Function = 
 // print(`Function: ${Function}`);
 
+Function;
+RangeError;
+Error;
+Object;
+SyntaxError;
+TypeError;
+ReferenceError;
+URIError;
+Symbol;
+EvalError;
+Number;
+parseFloat;
+Date;
+Boolean;
+BigInt;
+parseInt;
+WeakMap;
+RegExp;
+Set;
+Map;
+WeakRef;
+WeakSet;
+FinalizationRegistry;
+Array;
+Uint8ClampedArray;
+Uint8Array;
+TypedArray;
+Int8Array;
+Uint16Array;
+Uint32Array;
+Int16Array;
+Int32Array;
+Float32Array;
+Float64Array;
+BigInt64Array;
+BigUint64Array;
+SharedArrayBuffer;
+DataView;
+String;
+ArrayBuffer;
+eval;
+isFinite;
+ArkPrivate;
+print;
+decodeURI;
+decodeURIComponent;
+isNaN;
+encodeURI;
+NaN;
+globalThis;
+encodeURIComponent;
+Infinity;
+Math;
+JSON;
+Atomics;
+undefined;
+Reflect;
+Promise;
+Proxy;
+GeneratorFunction;
+Intl;
 
-function f26() {
-  function f29() {}
-  f29();
-  throw f29;
-}
-const v31 = f26();
-class C33 {}
+// .function any t.func_main_0(any a0, any a1, any a2) <static> {
+// 	tryldglobalbyname 0x0, "Function"
+// 	tryldglobalbyname 0x1, "RangeError"
+// 	tryldglobalbyname 0x2, "Error"
+// 	tryldglobalbyname 0x3, "Object"
+// 	tryldglobalbyname 0x4, "SyntaxError"
+// 	tryldglobalbyname 0x5, "TypeError"
+// 	tryldglobalbyname 0x6, "ReferenceError"
+// 	tryldglobalbyname 0x7, "URIError"
+// 	tryldglobalbyname 0x8, "Symbol"
+// 	tryldglobalbyname 0x9, "EvalError"
+// 	tryldglobalbyname 0xa, "Number"
+// 	tryldglobalbyname 0xb, "parseFloat"
+// 	tryldglobalbyname 0xc, "Date"
+// 	tryldglobalbyname 0xd, "Boolean"
+// 	tryldglobalbyname 0xe, "BigInt"
+// 	tryldglobalbyname 0xf, "parseInt"
+// 	tryldglobalbyname 0x10, "WeakMap"
+// 	tryldglobalbyname 0x11, "RegExp"
+// 	tryldglobalbyname 0x12, "Set"
+// 	tryldglobalbyname 0x13, "Map"
+// 	tryldglobalbyname 0x14, "WeakRef"
+// 	tryldglobalbyname 0x15, "WeakSet"
+// 	tryldglobalbyname 0x16, "FinalizationRegistry"
+// 	tryldglobalbyname 0x17, "Array"
+// 	tryldglobalbyname 0x18, "Uint8ClampedArray"
+// 	tryldglobalbyname 0x19, "Uint8Array"
+// 	tryldglobalbyname 0x1a, "TypedArray"
+// 	tryldglobalbyname 0x1b, "Int8Array"
+// 	tryldglobalbyname 0x1c, "Uint16Array"
+// 	tryldglobalbyname 0x1d, "Uint32Array"
+// 	tryldglobalbyname 0x1e, "Int16Array"
+// 	tryldglobalbyname 0x1f, "Int32Array"
+// 	tryldglobalbyname 0x20, "Float32Array"
+// 	tryldglobalbyname 0x21, "Float64Array"
+// 	tryldglobalbyname 0x22, "BigInt64Array"
+// 	tryldglobalbyname 0x23, "BigUint64Array"
+// 	tryldglobalbyname 0x24, "SharedArrayBuffer"
+// 	tryldglobalbyname 0x25, "DataView"
+// 	tryldglobalbyname 0x26, "String"
+// 	tryldglobalbyname 0x27, "ArrayBuffer"
+// 	tryldglobalbyname 0x28, "eval"
+// 	tryldglobalbyname 0x29, "isFinite"
+// 	tryldglobalbyname 0x2a, "ArkPrivate"
+// 	tryldglobalbyname 0x2b, "print"
+// 	tryldglobalbyname 0x2c, "decodeURI"
+// 	tryldglobalbyname 0x2d, "decodeURIComponent"
+// 	tryldglobalbyname 0x2e, "isNaN"
+// 	tryldglobalbyname 0x2f, "encodeURI"
+// 	ldnan
+// 	ldglobal
+// 	tryldglobalbyname 0x30, "encodeURIComponent"
+// 	ldinfinity
+// 	tryldglobalbyname 0x31, "Math"
+// 	tryldglobalbyname 0x32, "JSON"
+// 	tryldglobalbyname 0x33, "Atomics"
+// 	ldundefined
+// 	tryldglobalbyname 0x34, "Reflect"
+// 	tryldglobalbyname 0x35, "Promise"
+// 	tryldglobalbyname 0x36, "Proxy"
+// 	tryldglobalbyname 0x37, "GeneratorFunction"
+// 	tryldglobalbyname 0x38, "Intl"
+// 	ldundefined
+// 	returnundefined
+// }
