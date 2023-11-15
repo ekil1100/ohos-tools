fc035bf12109a8126888bf57d8dd1f59435bed54
like <zhenglike@huawei.com>
2023-11-15 15:47:06 +0800

    .

--- example.bash
+++ example.bash
@@ -7,8 +7,10 @@ export OHOS_IS_SA=false
 toggle_ohos_sa() {
     if [[ $OHOS_IS_SA == false ]]; then
         export OHOS_IS_SA=true
+        change_ohos_path standalone
     else
         export OHOS_IS_SA=false
+        change_ohos_path main
     fi
     echo "OHOS_IS_SA: $OHOS_IS_SA"
 }
